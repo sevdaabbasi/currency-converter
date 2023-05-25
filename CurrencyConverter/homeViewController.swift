ac3313db6ac7fa7780465da478cfdcd4489be21e
Sevda Abbasi <sevdaabbasi37@gmail.com>
2023-05-25 23:46:49 +0300

    user interface creation

---
+++ CurrencyConverter/homeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  homeViewController.swift
+//  CurrencyConverter
+//
+//  Created by Sevda Abbasi on 25.05.2023.
+//
+
+import UIKit
+
+class homeViewController: UIViewController{ //UITableViewDelegate, UITableViewDataSource {
+    
+   // func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //    return 10
+   // }
+    
+   // func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //    <#code#>
+   // }
+    
+   
+    
+   
+    
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+    }
+}
---
+++ CurrencyConverter/walletViewController.swift
@@ -0,0 +1,29 @@
+//
+//  walletViewController.swift
+//  CurrencyConverter
+//
+//  Created by Sevda Abbasi on 25.05.2023.
+//
+
+import UIKit
+
+class walletViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
